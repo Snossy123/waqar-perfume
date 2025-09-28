b058eae55ef152a6eff9e06c82e6815a572a720e
Snossy123 <soliemansnossy@gmail.com>
2025-09-29 00:16:08 +0300

    fix premission

--- database/migrations/2025_09_28_204249_create_products_table.php
+++ database/migrations/2025_09_28_204249_create_products_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->decimal('discounted_price', 8, 2)->nullable();
+            $table->integer('stars')->default(0);
+            $table->date('offer_end_date')->nullable();
+            $table->integer('stock')->default(0);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
